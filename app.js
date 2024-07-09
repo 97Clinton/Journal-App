a647c234ed3a50024c3395bd5fff7584b7467d64
user name <user email>
2024-07-09 15:54:58 +1200

    maybe

--- app.js
+++ app.js
@@ -19,7 +19,21 @@ app.use(express.static("public"));
 
 let posts = [];
 
-mongoose.connect("mongodb://127.0.0.1/blogDB", {useNewUrlParser: true});
+mongoose.connect(
+  'mongodb+srv://ClinJS:Clinton97@cluster0.5d81fz7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+).then(
+  () => {
+      console.log("DB connected successfully!");
+  }
+).catch(
+  (error) => {
+      console.log(error);
+      
+  }
+)
+
+// mongoose.connect("mongodb://127.0.0.1/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
