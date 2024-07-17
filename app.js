1c830c575dade387b4fed2208e552669a47fdb47
user name <user email>
2024-07-17 22:39:34 +1200

    still on it

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ app.get("/posts/:postId", function(req, res){
 
 // const port = process.env.PORT || 10000;
 
-app.listen(5888, function() {
-  console.log("Server started on port 5888");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
